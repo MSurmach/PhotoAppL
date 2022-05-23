a8f86709b9e67f15f285aa6c342b70c87897f98d
Maksim Surmach <m.surmach@godeltech.com>
2022-05-23 16:05:11 +0300

    Initial commit

---
+++ accounts-api/src/main/java/com/udemy/accounts/AccountsApiApplication.java
@@ -0,0 +1,15 @@
+package com.udemy.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AccountsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApiApplication.class, args);
+    }
+
+}
---
+++ accounts-api/src/test/java/com/udemy/accounts/AccauntsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccauntsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
